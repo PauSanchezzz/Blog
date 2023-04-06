21cb1ced8bf4e10d3f5049898ff2857698f06682
Paula Sanchez <pausanchezt12@gmail.com>
2023-04-05 21:36:12 -0500

    Se añaden imagenes y archivo js con el header

---
+++ Header.js
@@ -0,0 +1,16 @@
+const header= document.querySelector("#header");
+header.innerHTML = `<section class="header-icons-container">
+            <div class="icons">
+               <a href="/"><img src="imagenes/whatsapp.png" alt=""></a>
+                <a href="/"><img src="imagenes/instagram.png" alt=""></a>
+                <a href="/"><img src="imagenes/linkedin.png" alt=""></a> 
+            </div>
+        </section>
+    <nav>
+        <section class="nav-logo-container">
+            <a class="item-left" href="/"><img src="imagenes/Logo.png" alt="Logo del blog"></a>
+       </section>
+        <section class="profile-link"> 
+            <a class="item-right" href="perfil.html">Sobre Mi</a>
+        </section>
+    </nav>`;
